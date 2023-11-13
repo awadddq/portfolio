54b901522f2c1179caab1bb3c70e7a3ec1675c28
Awad A. Algarni <aalgarni@iotsquared.com.sa>
2023-11-13 10:12:15 +0300

    13nov

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,89 +1,46 @@
-(function($) {
-    'use strict';
-	
-	jQuery(document).ready(function(){
-		
-		/* START MENU JS */
-			$('a').on('click', function(e){
-				var anchor = $(this);
-				$('html, body').stop().animate({
-					scrollTop: $(anchor.attr('href')).offset().top - 50
-				}, 1500);
-				e.preventDefault();
-			});		
-			
-			$(window).on('scroll',function(){
-		    if($(this).scrollTop() > 200){
-		    	$('.main_header').addClass('sticky_menu');
-			}
-			else{
-			   $('.main_header').removeClass('sticky_menu');
-			}
-			});
-			$(document).on('click', '.navbar-collapse.in', function (e) {
-				if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-					$(this).collapse('hide');
-				}
-			});
-		/* END MENU JS */
-		
-		/* START ISOTOP JS */
-			var $grid = $('.work_content_area').isotope({
-			  // options
-			});
-			// filter items on button click
-			$('.work_filter').on( 'click', 'li', function() {
-			  var filterValue = $(this).attr('data-filter');
-			  $grid.isotope({ filter: filterValue });
-			});
-			// filter items on button click
-			$('.work_filter').on( 'click', 'li', function() {
-				$(this).addClass('active').siblings().removeClass('active')
-			});
-		/* END ISOTOP JS */
-		
-		/* START LIGHTBOX */
-		
-			lightbox.option({
-			  'resizeDuration': 200,
-			  'wrapAround': true
-			});
-		
-		/* END LIGHTBOX JS */
-		
-		/* START COUNDOWN JS */
-			$('#counter_area').on('inview', function(event, visible, visiblePartX, visiblePartY) {
-				if (visible) {
-					$(this).find('.counter').each(function () {
-						var $this = $(this);
-						$({ Counter: 0 }).animate({ Counter: $this.text() }, {
-							duration: 5000,
-							easing: 'swing',
-							step: function () {
-								$this.text(Math.ceil(this.Counter));
-							}
-						});
-					});
-					$(this).unbind('inview');
-				}
-			});
-		/* END COUNDOWN JS */
-		
-	});	
-	
-		/*PRELOADER JS*/
-			$(window).on('load', function() {  
-				$('.spinner').fadeOut();
-				$('.preloader').delay(350).fadeOut('slow'); 
-			}); 
-		/*END PRELOADER JS*/
-		
-		// Wow
-			new WOW().init();
-
-
-	$('.navbar-nav a').click(function(){
-        $('.navbar-collapse').removeClass('show in');
+/* ============================================================
+ * bootstrap-portfilter.js for Bootstrap v2.3.1
+ * https://github.com/geedmo/portfilter
+ * ============================================================*/
+! function(d) {
+    var c = "portfilter";
+    var b = function(e) {
+        this.$element = d(e);
+        this.stuff = d("[data-tag]");
+        this.target = this.$element.data("target") || ""
+    };
+    b.prototype.filter = function(g) {
+        var e = [],
+            f = this.target;
+        this.stuff.fadeOut("fast").promise().done(function() {
+            d(this).each(function() {
+                if (d(this).data("tag") == f || f == "all") {
+                    e.push(this)
+                }
+            });
+            d(e).show()
+        })
+    };
+    var a = d.fn[c];
+    d.fn[c] = function(e) {
+        return this.each(function() {
+            var g = d(this),
+                f = g.data(c);
+            if (!f) {
+                g.data(c, (f = new b(this)))
+            }
+            if (e == "filter") {
+                f.filter()
+            }
+        })
+    };
+    d.fn[c].defaults = {};
+    d.fn[c].Constructor = b;
+    d.fn[c].noConflict = function() {
+        d.fn[c] = a;
+        return this
+    };
+    d(document).on("click.portfilter.data-api", "[data-toggle^=portfilter]", function(f) {
+        d(this).portfilter("filter")
     })
-
-})(jQuery);
+}(window.jQuery);
